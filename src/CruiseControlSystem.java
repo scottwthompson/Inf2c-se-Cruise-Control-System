6c60003361cfe8be015a1d053d2c5337530cb644
kazzacarrot <kazzacarrot@hotmail.co.uk>
2014-11-05 13:14:01 +0000

    support code

---
+++ src/CruiseControlSystem.java
@@ -0,0 +1,17 @@
+/**
+ * This is the main class that you need to implement. You only have
+ * a single method to implement, but of course that may be easier if
+ * you define some auxiliary methods.
+ */
+public class CruiseControlSystem implements ICruiseControlSystem {
+	/*
+	 * Students may add any private fields or methods that they deem
+	 * necessary. Public ones should not be necessary (there is no
+	 * rule against it, but you should not be changing the support code
+	 * and the rest of the code knows only about this).
+	 */
+	
+	public void pulse(Car car){
+		/* Implement Something Here */
+	}
+}
